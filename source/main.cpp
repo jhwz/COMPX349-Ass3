9e309a83c2bb3c4bce4cb50fccb6b229b3cf40bb
ercb1 <elliottborehamnz@gmail.com>
2021-09-29 17:28:32 +1300

    Updated

--- source/main.cpp
+++ source/main.cpp
@@ -2,9 +2,14 @@
 
 MicroBit uBit;
 
+void drive();
+
 // event handelers
 #define CHANGE_PATH MICROBIT_ID_NOTIFY + 1
 #define CHANGE_PATH_EVT 0
+
+int leftLT = 0;
+int rightLT = 0;
 
 enum LED
 {
@@ -40,6 +45,7 @@ void displayStatus(char i)
         uBit.display.image.setPixelValue(2, 1, 255);
         uBit.display.image.setPixelValue(2, 2, 255);
         uBit.display.image.setPixelValue(2, 3, 255);
+        uBit.display.image.setPixelValue(2, 4, 255);
     }
     else if (i == 'b')
     {
@@ -62,6 +68,7 @@ void displayStatus(char i)
         uBit.display.image.setPixelValue(1, 2, 255);
         uBit.display.image.setPixelValue(2, 2, 255);
         uBit.display.image.setPixelValue(3, 2, 255);
+        uBit.display.image.setPixelValue(0, 2, 255);
     }
     else if (i == 'r')
     {
@@ -73,6 +80,7 @@ void displayStatus(char i)
         uBit.display.image.setPixelValue(1, 2, 255);
         uBit.display.image.setPixelValue(2, 2, 255);
         uBit.display.image.setPixelValue(3, 2, 255);
+        uBit.display.image.setPixelValue(4, 2, 255);
     }
 }
 
@@ -81,11 +89,11 @@ void forward()
     uint8_t buf[3];
     buf[0] = 0x00;
     buf[1] = 0x00;
-    buf[2] = 0x60;
+    buf[2] = 0x30;
     uBit.i2c.write(0x20, buf, 3);
     buf[0] = 0x02;
     buf[1] = 0x00;
-    buf[2] = 0x60;
+    buf[2] = 0x30;
     uBit.i2c.write(0x20, buf, 3);
     displayStatus('f');
 }
@@ -94,11 +102,11 @@ void reverse()
     uint8_t buf[3];
     buf[0] = 0x00;
     buf[1] = 0x01;
-    buf[2] = 0x60;
+    buf[2] = 0x40;
     uBit.i2c.write(0x20, buf, 3);
     buf[0] = 0x02;
     buf[1] = 0x01;
-    buf[2] = 0x60;
+    buf[2] = 0x40;
     uBit.i2c.write(0x20, buf, 3);
     displayStatus('b');
 }
@@ -107,11 +115,11 @@ void right()
     uint8_t buf[3];
     buf[0] = 0x00;
     buf[1] = 0x00;
-    buf[2] = 0x40;
+    buf[2] = 0x20;
     uBit.i2c.write(0x20, buf, 3);
     buf[0] = 0x02;
-    buf[1] = 0x00;
-    buf[2] = 0x00;
+    buf[1] = 0x01;
+    buf[2] = 0x20;
     uBit.i2c.write(0x20, buf, 3);
     displayStatus('r');
 }
@@ -119,12 +127,12 @@ void left()
 {
     uint8_t buf[3];
     buf[0] = 0x00;
-    buf[1] = 0x00;
-    buf[2] = 0x00;
+    buf[1] = 0x10;
+    buf[2] = 0x20;
     uBit.i2c.write(0x20, buf, 3);
     buf[0] = 0x02;
     buf[1] = 0x00;
-    buf[2] = 0x40;
+    buf[2] = 0x20;
     uBit.i2c.write(0x20, buf, 3);
     displayStatus('l');
 }
@@ -151,52 +159,48 @@ int readLine(Line line)
 void onLeftLineOn(MicroBitEvent evt)
 {
     setLED(LED::LEFT, true);
+    leftLT = 1;
+    drive();
 }
 
 void onLeftLineOff(MicroBitEvent evt)
 {
     setLED(LED::LEFT, false);
+    leftLT = 0;
+    drive();
 }
 
 void onRightLineOn(MicroBitEvent evt)
 {
     setLED(LED::RIGHT, true);
+    rightLT = 1;
+    drive();
 }
 
 void onRightLineOff(MicroBitEvent evt)
 {
     setLED(LED::RIGHT, false);
+    rightLT = 0;
+    drive();
+}
+
+void drive(){
+    uBit.serial.printf("updateing trajectory\n\r");
+    if (!leftLT && rightLT){forward();}
+    if (!leftLT && !rightLT){right();}
+    if (leftLT && rightLT){left();}
 }
 
 int main()
 {
     uBit.init();
+    uBit.serial.printf("starting\n\r");
     uBit.io.P13.eventOn(DEVICE_PIN_EVENT_ON_EDGE);
     uBit.io.P14.eventOn(DEVICE_PIN_EVENT_ON_EDGE);
-
-    for (;;)
-    {
-        if (readLine(Line::LEFTSensor) == 1)
-        {
-            setLED(LED::LEFT, true);
-        }
-        else
-        {
-            setLED(LED::LEFT, false);
-        }
-        if (readLine(Line::RIGHTSensor) == 1)
-        {
-            setLED(LED::RIGHT, true);
-        }
-        else
-        {
-            setLED(LED::RIGHT, false);
-        }
-    }
     uBit.messageBus.listen(uBit.io.P13.id, DEVICE_PIN_EVT_RISE, onLeftLineOn);
     uBit.messageBus.listen(uBit.io.P13.id, DEVICE_PIN_EVT_FALL, onLeftLineOff);
     uBit.messageBus.listen(uBit.io.P14.id, DEVICE_PIN_EVT_RISE, onRightLineOn);
     uBit.messageBus.listen(uBit.io.P14.id, DEVICE_PIN_EVT_FALL, onRightLineOff);
-
+    //uBit.messageBus.listen(CHANGE_PATH MICROBIT, CHANGE_PATH_EVT, drive);
     release_fiber();
 }
