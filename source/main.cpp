b9b2a94f8b4a3876e60fc2f103f949fb5b05eaf2
Joel Howse <joelhowse@gmail.com>
2021-10-17 17:09:09 +1300

    general changes

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,21 @@
 #include "MicroBit.h"
-// #include "robot.cpp"
 
+// Global microbit structure for interfacing with the microbit board
+MicroBit uBit;
+
+/*
+ * FEATURE FLAGS
+ */
+
+// If set will alternate directions at intersections, otherwise will go straight through
 #define INTERSECTION_ALTERNATE 1
 
-MicroBit uBit;
+// If set will enable the sonar in the robot
+#define USE_SONAR 0
+
+/*
+ * HARDWARE BASED FUNCTIONS
+ */
 
 // Enum for all of the LED types. There are two LEDs on the maqueen.
 enum LED
@@ -17,13 +29,9 @@ enum LED
 void setLED(LED led, bool on)
 {
     if (led == LED::LED_LEFT || led == LED::LED_ALL)
-    {
         uBit.io.P8.setDigitalValue(on ? 1 : 0);
-    }
     else if (led == LED::LED_RIGHT || led == LED::LED_ALL)
-    {
         uBit.io.P12.setDigitalValue(on ? 1 : 0);
-    }
 }
 
 // enum for addressing the two linesensors
@@ -33,6 +41,7 @@ enum LineSensor
     LS_RIGHT = 2,
 };
 
+// readLine reads the value at the given line sensor
 int readLine(LineSensor line)
 {
     if (line == LineSensor::LS_LEFT)
@@ -42,7 +51,7 @@ int readLine(LineSensor line)
     return -1;
 }
 
-// Enum for addressing the wheels.
+// Enum for addressing each of the two wheels.
 enum Wheel
 {
     WHEEL_LEFT = 0x00,
@@ -67,289 +76,259 @@ void moveWheel(Wheel wheel, uint8_t speed, WheelDirection dir)
     uBit.i2c.write(0x20, buf, 3);
 }
 
+#if USE_SONAR
 // does a cycle of the ultrasonic reader, returning the distance in cm
 int readUlt()
 {
-    int d;
+    // send a pulse out. Timings found on a microbit/maqueen reference
     uBit.io.P1.setDigitalValue(0);
     uBit.sleep(2);
     uBit.io.P1.setDigitalValue(1);
     uBit.sleep(10);
     uBit.io.P1.setDigitalValue(0);
 
+    // listen for a high pulse on the second pin
     uBit.io.P2.setPolarity(1);
-    d = uBit.io.P2.getPulseUs(250);
+    int d = uBit.io.P2.getPulseUs(250);
     return d > 0 ? d / 58 : d;
 }
-
-void displayStatus(char i)
-{
-    uBit.display.image.clear();
-    if (i == 'f')
-    {
-        uBit.display.image.setPixelValue(0, 2, 255);
-        uBit.display.image.setPixelValue(1, 1, 255);
-        uBit.display.image.setPixelValue(2, 0, 255);
-        uBit.display.image.setPixelValue(3, 1, 255);
-        uBit.display.image.setPixelValue(4, 2, 255);
-        uBit.display.image.setPixelValue(2, 1, 255);
-        uBit.display.image.setPixelValue(2, 2, 255);
-        uBit.display.image.setPixelValue(2, 3, 255);
-        uBit.display.image.setPixelValue(2, 4, 255);
-    }
-    else if (i == 'b')
-    {
-        uBit.display.image.setPixelValue(0, 2, 255);
-        uBit.display.image.setPixelValue(1, 3, 255);
-        uBit.display.image.setPixelValue(2, 4, 255);
-        uBit.display.image.setPixelValue(3, 3, 255);
-        uBit.display.image.setPixelValue(4, 2, 255);
-        uBit.display.image.setPixelValue(2, 1, 255);
-        uBit.display.image.setPixelValue(2, 2, 255);
-        uBit.display.image.setPixelValue(2, 3, 255);
-    }
-    else if (i == 'l')
-    {
-        uBit.display.image.setPixelValue(2, 0, 255);
-        uBit.display.image.setPixelValue(3, 1, 255);
-        uBit.display.image.setPixelValue(4, 2, 255);
-        uBit.display.image.setPixelValue(3, 3, 255);
-        uBit.display.image.setPixelValue(2, 4, 255);
-        uBit.display.image.setPixelValue(1, 2, 255);
-        uBit.display.image.setPixelValue(2, 2, 255);
-        uBit.display.image.setPixelValue(3, 2, 255);
-        uBit.display.image.setPixelValue(0, 2, 255);
-    }
-    else if (i == 'r')
-    {
-        uBit.display.image.setPixelValue(2, 0, 255);
-        uBit.display.image.setPixelValue(1, 1, 255);
-        uBit.display.image.setPixelValue(0, 2, 255);
-        uBit.display.image.setPixelValue(1, 3, 255);
-        uBit.display.image.setPixelValue(2, 4, 255);
-        uBit.display.image.setPixelValue(1, 2, 255);
-        uBit.display.image.setPixelValue(2, 2, 255);
-        uBit.display.image.setPixelValue(3, 2, 255);
-        uBit.display.image.setPixelValue(4, 2, 255);
-    }
-}
-
-struct State
-{
-    int8_t prevLeftLS;
-    int8_t prevRightLS;
-    int8_t leftLS;
-    int8_t rightLS;
-
-    bool stopped;
-
-    WheelDirection forward;
-    WheelDirection reverse;
-    Wheel left;
-    Wheel right;
-
-    // intersection detection
-    bool searching;
-    unsigned long enableIntersectionsAfter;
-
-#if INTERSECTION_ALTERNATE
-    // if true, goes right at an intersection. Otherwise it goes left
-    bool goRightAtIntersection;
 #endif
+
+/*
+ *
+ * GLOBAL STATE
+ *
+ */
+
+// different states the robot will be in
+enum State
+{
+    FOLLOW_LINE = 0,
+    STOPPED = 1,
+    COMPUTING = 2,
 };
 
-State state = {};
+// current state of the robot
+State state = FOLLOW_LINE;
+bool recompute = false;
 
+// current values of the line sensors. These are updated by the
+// line sensor events
+int8_t leftLS = 0;
+int8_t rightLS = 0;
+
+// level of abstraction on what the robot thinks the directions are.
+// allows us to modify these values to make the robot go backwards instead
+// of forward for example
+WheelDirection forward = WHEEL_FORWARD;
+WheelDirection reverse = WHEEL_BACKWARD;
+Wheel left = WHEEL_LEFT;
+Wheel right = WHEEL_RIGHT;
+
+// if true, goes right at an intersection. Otherwise it goes left.
+// only need this value if doing the intersection logic
+#if INTERSECTION_ALTERNATE
+bool goRightAtIntersection = false;
+#endif
+
+/*
+ *
+ * DOMAIN LOGIC
+ *
+ */
+
+// This function is responsible for all of the driving logic. All of the events call this function
+// to create a response to the state change the robot may have given
 void drive()
 {
-    // no driving logic - currently doing intersection detection
-    if (state.searching)
+    // currently stopped - ignore event
+    if (state == State::STOPPED)
         return;
 
-    if (state.stopped)
+    // another routine is running - set recompute flag and return
+    if (state == State::COMPUTING)
     {
-        moveWheel(WHEEL_LEFT, 0, WHEEL_FORWARD);
-        moveWheel(WHEEL_RIGHT, 0, WHEEL_FORWARD);
-        state.prevLeftLS = -1;
-        state.prevRightLS = -1;
+        recompute = true;
         return;
     }
-    else if (state.prevLeftLS == state.leftLS && state.prevRightLS == state.rightLS)
-        return;
+    state = State::COMPUTING;
 
-    if (!state.leftLS && !state.rightLS)
+    if (!leftLS && !rightLS)
     {
-        moveWheel(state.left, 32, state.forward);
-        moveWheel(state.right, 32, state.forward);
+        moveWheel(left, 28, forward);
+        moveWheel(right, 28, forward);
     }
-    else if (state.leftLS && !state.rightLS)
+#define ADJUST_SPEED 20 // put an inline define to make tweaking values quicker
+    else if (leftLS && !rightLS)
     {
-        moveWheel(state.left, 24, state.forward);
-        moveWheel(state.right, 24, state.reverse);
+        moveWheel(left, ADJUST_SPEED, forward);
+        moveWheel(right, ADJUST_SPEED, reverse);
     }
-    else if (!state.leftLS && state.rightLS)
+    else if (!leftLS && rightLS)
     {
-        moveWheel(state.right, 24, state.forward);
-        moveWheel(state.left, 24, state.reverse);
+        moveWheel(right, ADJUST_SPEED, forward);
+        moveWheel(left, ADJUST_SPEED, reverse);
     }
     else
     {
-        // Disable intersection detection for a bit
-        if (uBit.systemTime() < state.enableIntersectionsAfter)
-            return;
 
-        uBit.serial.printf("intersection detection\n\r");
-        // try detect an intersection
-        bool intersection = false;
-        state.searching = true;
-        const int speed = 28;
-        const int sleep = 300;
         // go forward to test if its an intersection
-        moveWheel(state.left, speed, state.forward);
-        moveWheel(state.right, speed, state.forward);
-        uBit.sleep(sleep);
-        intersection = !readLine(LS_LEFT) || !readLine(LS_RIGHT);
-        if (intersection)
-        {
-#if INTERSECTION_ALTERNATE
-            moveWheel(state.left, speed, state.reverse);
-            moveWheel(state.right, speed, state.reverse);
-            uBit.sleep(sleep);
+        moveWheel(left, 24, forward);
+        moveWheel(right, 24, forward);
+        uBit.sleep(350);
 
-            if (state.goRightAtIntersection)
+        if (!readLine(LS_LEFT) || !readLine(LS_RIGHT))
+        {
+            // We think this is an intersection, perform that logic
+            moveWheel(left, 0, forward);
+            moveWheel(right, 0, forward);
+            uBit.sleep(2000);
+
+#if INTERSECTION_ALTERNATE
+            if (goRightAtIntersection)
             {
-                moveWheel(state.left, speed, state.forward);
-                moveWheel(state.right, speed, state.reverse);
+                moveWheel(left, 30, forward);
+                moveWheel(right, 25, reverse);
             }
             else
             {
-                moveWheel(state.right, speed, state.forward);
-                moveWheel(state.left, speed, state.reverse);
+                moveWheel(right, 30, forward);
+                moveWheel(left, 25, reverse);
             }
-            uBit.sleep(500);
-            moveWheel(state.right, speed, state.forward);
-            moveWheel(state.left, speed, state.forward);
-            uBit.sleep(300);
-            state.goRightAtIntersection = !state.goRightAtIntersection;
+            uBit.sleep(800);
+
+            // toggle the direction we choose
+            goRightAtIntersection = !goRightAtIntersection;
 #else
-            moveWheel(state.left, speed, state.forward);
-            moveWheel(state.right, speed, state.forward);
+            moveWheel(left, speed, forward);
+            moveWheel(right, speed, forward);
 #endif
         }
         else
         {
-            moveWheel(state.left, speed, state.reverse);
-            moveWheel(state.right, speed, state.reverse);
-            uBit.sleep(sleep);
+            // This isn't an intersection, try treat it as a sharp corner
 
-            // try go right
-            moveWheel(state.right, speed, state.forward);
-            moveWheel(state.left, speed, state.reverse);
-            uBit.sleep(sleep * 2);
-            moveWheel(state.right, 0, state.forward);
-            moveWheel(state.left, 0, state.reverse);
+            // Move back to the line, we moved forward just before to test if this was an
+            // intersection. I found it works best to not move all the way back
+            moveWheel(left, 24, reverse);
+            moveWheel(right, 24, reverse);
+            uBit.sleep(200);
+
+            // move the robot to the left a bit. If we find a line then assume the corner goes that
+            // way and follow it around
+            moveWheel(right, 30, forward);
+            moveWheel(left, 30, reverse);
+            uBit.sleep(700);
+
+            // maybe sample a few times over the time period so we can check if we passed over the
+            // line?
+
+            // if both line sensors are true then we aren't on a line, move back
+            // this will leave the robot in the turning state so worst case, it
+            // will turn all the way back to the line it was on before. This accounts for the end of
+            // line states
             if (readLine(LS_LEFT) && readLine(LS_RIGHT))
             {
-                moveWheel(state.left, speed, state.forward);
-                moveWheel(state.right, speed, state.reverse);
-                uBit.sleep(sleep * 2);
-            }
-            else
-            {
-                moveWheel(state.right, speed, state.forward);
-                moveWheel(state.left, speed, state.reverse);
+                moveWheel(left, 30, forward);
+                moveWheel(right, 30, reverse);
             }
         }
-
-        // only bother doing this again after a second
-        state.enableIntersectionsAfter = uBit.systemTime() + 1000;
-        state.searching = false;
     }
-    state.prevLeftLS = state.leftLS;
-    state.prevRightLS = state.rightLS;
+    state = State::FOLLOW_LINE;
+    if (recompute)
+    {
+        recompute = false;
+        drive();
+    }
 }
 
-//
-// Events
-//
+/*
+ *
+ * EVENT HANDLERS
+ *
+ */
 
 void onLeftLineOn(MicroBitEvent evt)
 {
     setLED(LED::LED_LEFT, true);
-    state.leftLS = 1;
+    leftLS = 1;
     drive();
 }
 
 void onLeftLineOff(MicroBitEvent evt)
 {
     setLED(LED::LED_LEFT, false);
-    state.leftLS = 0;
+    leftLS = 0;
     drive();
 }
 
 void onRightLineOn(MicroBitEvent evt)
 {
     setLED(LED::LED_RIGHT, true);
-    state.rightLS = 1;
+    rightLS = 1;
     drive();
 }
 
 void onRightLineOff(MicroBitEvent evt)
 {
     setLED(LED::LED_RIGHT, false);
-    state.rightLS = 0;
+    rightLS = 0;
     drive();
 }
 
+#if USE_SONAR
+// gets called every 250ms
 void timerTick(MicroBitEvent)
 {
+    if (state != State::FOLLOW_LINE && state != State::STOPPED)
+        return;
+
+    // read the ultrasonic sensor
     int d = readUlt();
-    // uBit.serial.printf("ultrasonic: %d\n\r", d);
+
+    // only do something if a valid value was read, is negative if not able to determine
     if (d > 0)
     {
-        bool callDrive = false;
-        if (state.stopped != (5 <= d && d < 10))
+        if (state == State::STOPPED && d >= 10)
+            state = State::FOLLOW_LINE;
+
+        if (state != State::STOPPED && (5 <= d && d < 10))
         {
-            state.stopped = !state.stopped;
-            callDrive = true;
+            moveWheel(WHEEL_LEFT, 0, WHEEL_FORWARD);
+            moveWheel(WHEEL_RIGHT, 0, WHEEL_FORWARD);
+            state = State::STOPPED;
         }
-        if (state.forward != (d < 5 ? WHEEL_BACKWARD : WHEEL_FORWARD))
+
+        if (forward != (d < 5 ? WHEEL_BACKWARD : WHEEL_FORWARD))
         {
+            if (state == State::STOPPED)
+                state = State::FOLLOW_LINE;
+
             if (d < 5)
             {
-                state.forward = WHEEL_BACKWARD;
-                state.reverse = WHEEL_FORWARD;
-                state.left = WHEEL_RIGHT;
-                state.right = WHEEL_LEFT;
+                forward = WHEEL_BACKWARD;
+                reverse = WHEEL_FORWARD;
+                left = WHEEL_RIGHT;
+                right = WHEEL_LEFT;
             }
             else
             {
-                state.forward = WHEEL_FORWARD;
-                state.reverse = WHEEL_BACKWARD;
-                state.left = WHEEL_LEFT;
-                state.right = WHEEL_RIGHT;
+                forward = WHEEL_FORWARD;
+                reverse = WHEEL_BACKWARD;
+                left = WHEEL_LEFT;
+                right = WHEEL_RIGHT;
             }
-            callDrive = true;
         }
-        if (callDrive)
-            drive();
     }
 }
+#endif
 
+// The main function registers the event handlers and then lets microbit handle things from there
 int main()
 {
     uBit.init();
-    uBit.serial.printf("starting\n\r");
 
-    // explicitly initilise state
-    state.prevLeftLS = -1;
-    state.prevRightLS = -1;
-    state.forward = WHEEL_FORWARD;
-    state.reverse = WHEEL_BACKWARD;
-    state.left = WHEEL_LEFT;
-    state.right = WHEEL_RIGHT;
-
-    // register handlers for our events
+    // register handlers for the line sensor events
     uBit.io.P13.eventOn(DEVICE_PIN_EVENT_ON_EDGE);
     uBit.io.P14.eventOn(DEVICE_PIN_EVENT_ON_EDGE);
     uBit.messageBus.listen(uBit.io.P13.id, DEVICE_PIN_EVT_RISE, onLeftLineOn);
@@ -357,8 +336,15 @@ int main()
     uBit.messageBus.listen(uBit.io.P14.id, DEVICE_PIN_EVT_RISE, onRightLineOn);
     uBit.messageBus.listen(uBit.io.P14.id, DEVICE_PIN_EVT_FALL, onRightLineOff);
 
+#if USE_SONAR
+    // register an event to occur every 250 ms. We use this for the ultrasonic sensor.
     uBit.timer.eventEvery(250, 5000, DEVICE_EVT_ANY, CODAL_TIMER_EVENT_FLAGS_NONE);
     uBit.messageBus.listen(5000, DEVICE_EVT_ANY, timerTick);
+#endif
+
+    // bootstrap the movements by calling drive()
+    // this should just make the robot go straight forward
+    drive();
 
     release_fiber();
 }
